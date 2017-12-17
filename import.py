5fc44106d6b7a39ff863a68d147605efd23882f2
lily20171217 <444896010@qq.com>
2017-12-17 18:05:51 +0800

    add import.py

---
+++ import.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from lib.utils import *
+from lib.printinfo import printinfo
+# from lib.utils import printme
+
+print(sum(100))
+print("hello world")
+print(printinfo("sally",16))
